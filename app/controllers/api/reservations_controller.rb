03d0e0105da2ec68aa65940ff00a46e51daa1a39
anyars-encarta <anyarsencarta@gmail.com>
2024-01-18 13:25:17 +0000

    Create API Endpoints for Users, Rooms and Reservations

--- app/controllers/api/reservations_controller.rb
+++ app/controllers/api/reservations_controller.rb
@@ -1,2 +1,22 @@
 class Api::ReservationsController < ApplicationController
+    def index
+        @reservations = Reservation.all
+        render json: @reservations
+    end
+
+    def create
+        user = User.find(params[:user_id])
+        room = Room.find(params[:room_id])
+      
+        reservation = Reservation.new(
+          user: user,
+          room: room,
+          date: params[:date],
+          city: params[:city]
+        )
+      
+        reservation.save
+      
+        render json: reservation
+      end
 end
--- app/controllers/api/rooms_controller.rb
+++ app/controllers/api/rooms_controller.rb
@@ -1,2 +1,6 @@
 class Api::RoomsController < ApplicationController
+    def rooms
+        rooms = Room.all
+        render json: rooms
+    end
 end
