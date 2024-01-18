9639021a02452b6f910afdc1ab5ca4984154edcf
anyars-encarta <anyarsencarta@gmail.com>
2024-01-18 11:27:56 +0000

    Correct rubocop errors

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,9 +1,9 @@
 class RoomsController < ApplicationController
-    before_action :require_admin, only: [:create, :show, :update, :destroy]
+  before_action :require_admin, only: %i[create show update destroy]
 
-    private
+  private
 
-    def require_admin
-      redirect_to root_path unless current_user.admin?
-    end
+  def require_admin
+    redirect_to root_path unless current_user.admin?
+  end
 end
