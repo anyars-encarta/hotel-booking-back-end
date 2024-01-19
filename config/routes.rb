39d06d2cfcd80b70f5f8c00423b4106156308b47
Mozamel Jawad <mozameljawad@gmail.com>
2024-01-19 12:29:31 +0430

    add route for room details information

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@ Rails.application.routes.draw do
   namespace :api do
     get 'users', to: 'users#users'
     get 'rooms', to: 'rooms#rooms'
-    
+    get 'rooms/:id', to: 'rooms#show'
+
     resources :users, only: [:index, :create, :update, :destroy]
     resources :rooms, only: [:index, :create, :update, :destroy]
     resources :reservations, only: [:index, :create, :update, :destroy]
