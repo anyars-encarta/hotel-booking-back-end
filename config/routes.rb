93a59977fc4adfeddf4e5614efaab14f7d4f5e22
Mozamel Jawad <mozameljawad@gmail.com>
2024-01-19 21:29:13 +0430

    update route

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   # root "posts#index"
 
-  namespace :api do   
+  namespace :api do
     get 'users/:id', to: 'users#index'
-   
+
     resources :users, only: [:index, :create, :update, :destroy]
-    resources :rooms, only: [:index, :create, :update, :destroy]
+    resources :rooms, only: [:index, :show, :create, :update, :destroy]
     resources :reservations, only: [:index, :create, :update, :destroy]
   end
 end
