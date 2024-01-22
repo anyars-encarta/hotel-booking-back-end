4348700a7145bb7288a8763ef0ac947eeb4774d8
coker2019 <olabisi2k11@gmail.com>
2024-01-22 15:28:02 +0100

    update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,21 @@
 Rails.application.routes.draw do
+  get '/current_user', to: 'current_user#index'
+  devise_for :users, path: '', path_names: {
+    sign_in: 'login',
+    sign_out: 'logout',
+    registration: 'signup'
+  },
+  controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
   # root "posts#index"
 
   namespace :api do
     get 'users', to: 'users#users'
     get 'rooms', to: 'rooms#rooms'
     
-    resources :users, only: [:index, :create, :update, :destroy]
+    resources :users, only: [:create]
     resources :rooms, only: [:index, :create, :update, :destroy]
     resources :reservations, only: [:index, :create, :update, :destroy]
   end
