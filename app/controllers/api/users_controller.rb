1011418c6c1784279bc3dbcd848d25383a4064b3
Mozamel Jawad <mozameljawad@gmail.com>
2024-01-28 11:03:10 +0430

    remove comments

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,22 +1,15 @@
 class Api::UsersController < ApplicationController
   before_action :set_user, only: %i[show update destroy]
 
-  # GET /users
-  # GET /users.json
   def index
     @users = User.all
     render json: @users
   end
 
-  # GET /users/1
-  # GET /users/1.json
-
   def show
     render json: @user
   end
 
-  # POST /users
-  # POST /users.json
   def create
     @user = User.new(user_params)
 
@@ -27,8 +20,7 @@ class Api::UsersController < ApplicationController
     end
   end
 
-  # PATCH/PUT /users/1
-  # PATCH/PUT /users/1.json
+
   def update
     if @user.update(user_params)
       render :show, status: :ok
@@ -37,20 +29,17 @@ class Api::UsersController < ApplicationController
     end
   end
 
-  # DELETE /users/1
-  # DELETE /users/1.json
+
   def destroy
     @user.destroy!
   end
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
   def set_user
     @user = User.find(params[:id])
   end
 
-  # Only allow a list of trusted parameters through.
   def user_params
     params.require(:user).permit(:username, :email, :password, :password_confirmation)
   end
