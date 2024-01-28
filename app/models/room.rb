b380ddb49c09f92486ea0d84fcd31ae2d1205f05
Mozamel Jawad <mozameljawad@gmail.com>
2024-01-28 10:59:25 +0430

    add validation to room model

--- app/models/room.rb
+++ app/models/room.rb
@@ -2,6 +2,9 @@ class Room < ApplicationRecord
   belongs_to :category
   has_many :reservations
 
+  validates :name, presence: true
+  validates :reserved, inclusion: { in: [true, false] }
+
   def update_reserve
     update(reserved: !reserved)
   end
