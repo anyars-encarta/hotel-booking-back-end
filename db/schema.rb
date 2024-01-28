0bc8da6aea8e02d5697ff2d60ccb794c6fd6ab85
anyars-encarta <anyarsencarta@gmail.com>
2024-01-28 13:54:09 +0000

    Correct schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_20_092005) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_26_192930) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -36,9 +36,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_20_092005) do
 
   create_table "rooms", force: :cascade do |t|
     t.string "name"
-    t.string "room_type"
-    t.text "description"
-    t.string "image"
+    t.integer "category_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "reserved", default: false
