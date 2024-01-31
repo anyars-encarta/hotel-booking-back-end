4d4b7720449ee803e5333c5f336a282032c9686a
Mozamel Jawad <mozameljawad@gmail.com>
2024-01-31 11:22:34 +0430

    add seeded data

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,15 @@
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
 # Users
+
+# Categories
+cat1 = Category.create(name: 'Standard', description: 'standard room with A/C, TV, and double-size bed', image: 'https://www.eliaermouhotel.com/uploads/photos/D1024/2019/02/standardroom_1878.jpg', number_of_rooms: 0, number_reserved: 0, price: 120)
+cat2 = Category.create(name: 'Executive', description: 'Executive room with A/C, TV, Fridge, and queen- size bed', image: 'https://www.strandhotellimerick.ie/wp-content/uploads/2021/05/Main_Bedroom-Executive-Double-1-1366x768-fp_mm-fpoff_0_0.jpg', number_of_rooms: 0, number_reserved: 0, price: 250)
+
+# Rooms
+room1 = Room.create(name: 'Room 1', category_id: cat1.id, reserved: false)
+room2 = Room.create(name: 'Room 2', category_id: cat1.id, reserved: false)
+room3 = Room.create(name: 'Room 3', category_id: cat2.id, reserved: false)
+
+# message
+puts 'Seeded 2 categories and 3 rooms'
